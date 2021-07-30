7b0a8e48efd6442e7755215b696ada8bb3627cd9
Queenie <73581687+queenie-ho@users.noreply.github.com>
2021-07-31 00:18:01 +0800

    Add files via upload

---
+++ huawei/all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash 
+gnome-terminal -x bash -c "./play1.sh;exec bash;"
+gnome-terminal -x bash -c "./play2.sh;exec bash;"
+gnome-terminal -x bash -c "./record-a.sh;exec bash;"
+# gnome-terminal -x bash -c "./play-r.sh;exec bash;"
+# gnome-terminal -x bash -c "./record-o.sh;exec bash;"
+# gnome-terminal -x bash -c "./record-a.sh;exec bash;"
